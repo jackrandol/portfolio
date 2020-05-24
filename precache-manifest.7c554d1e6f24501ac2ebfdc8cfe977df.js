0692230004df8d555a72047eccc8d52865784ba9
Jack Randol <jackrandol@gmail.com>
2020-05-24 17:38:32 +0200

    Updates

---
+++ precache-manifest.7c554d1e6f24501ac2ebfdc8cfe977df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d90e1012c640605d1abe9e1ffb6bef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e552d5c9ff49ee9c08b5",
+    "url": "/portfolio/static/css/main.727b17bc.chunk.css"
+  },
+  {
+    "revision": "b8ce1235e31761deaad6",
+    "url": "/portfolio/static/js/2.a1167eb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a1167eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e552d5c9ff49ee9c08b5",
+    "url": "/portfolio/static/js/main.deb767f5.chunk.js"
+  },
+  {
+    "revision": "8254d576b134234d3a55",
+    "url": "/portfolio/static/js/runtime-main.1b8931a1.js"
+  },
+  {
+    "revision": "b63f9505dd8b916db8cf5db6efe843f4",
+    "url": "/portfolio/static/media/JRWrapper2.b63f9505.jpg"
+  }
+]);
